80f9f968b325aed50afebb84e617c4a2633d75b5
ayushtomarpsl <ayushtomarpsl@gmail.com>
2024-11-08 09:44:19 +0000

    jhsdfj

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // {
-    //     path: '',
-    //     loadComponent:()=>import('./common-popup/common-popup.component').then(t=>t.CommonPopupComponent)
-    // }
 ];
